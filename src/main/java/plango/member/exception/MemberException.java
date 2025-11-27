752aee03a1f047f9cbda21ae6a748a0511ab8238
katejhee <katekwak1@gachon.ac.kr>
2025-11-28 02:47:42 +0900

    Update MemberException.java

--- src/main/java/plango/member/exception/MemberException.java
+++ src/main/java/plango/member/exception/MemberException.java
@@ -14,19 +14,3 @@ public class MemberException extends BusinessException {
     }
 }
 
-package plango.member.exception;
-
-import lombok.Getter;
-import plango.global.common.exception.BusinessException;
-
-@Getter
-public class MemberException extends BusinessException {
-
-    private final MemberErrorCode errorCode;
-
-    public MemberException(MemberErrorCode errorCode) {
-        super(errorCode.getStatus().value(), errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
-}
-
