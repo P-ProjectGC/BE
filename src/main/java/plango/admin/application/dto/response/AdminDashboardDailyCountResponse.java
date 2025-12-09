f10d699d9c7dd40c1bb524b9913f6b3769be7ded
katejhee <katekwak1@gachon.ac.kr>
2025-12-09 20:02:09 +0900

    불필요 파일 삭제

--- src/main/java/plango/admin/application/dto/response/AdminDashboardDailyCountResponse.java
+++
@@ -1,9 +0,0 @@
-package plango.admin.application.dto.response;
-
-import java.time.LocalDate;
-
-public record AdminDashboardDailyCountResponse(
-        LocalDate date,
-        long newMemberCount
-) {}
-
